b8e0af39f3af1d891dcd6e1242a3af710e4afd16
darho <darho@ap.equinix.com>
2022-05-26 15:04:54 +0800

    Create testing POST method for trade

--- pages/api/trade.tsx
+++ pages/api/trade.tsx
@@ -1,25 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { connectToDatabase } from '../../middleware/database_test';
-import Trade from '../../models/Trade'
+import { connectToDatabase } from '../../middleware/database';
+// import Trade from '../../models/Trade'
 
 
 export default async function handler(req: NextApiRequest,res: NextApiResponse) {
-    if(req.method == "POST"){
-        try{
-            const trade = await Trade.create(req.body);
-            res.status(201).json(trade);
-        }catch(err){
-            res.status(500).json(err);
+
+    switch (req.method){
+        case "POST":{
+            try{
+                let { db } = await connectToDatabase();
+                const tradeObj =   await  {"trend":
+                {  
+                    "fourHourTrend":req.body.fourHourTrend,
+                    "oneDayTrend":req.body.oneDayTrend
+                }}
+                const trade = await db.collection('checklist').insertOne(tradeObj)
+                console.log(trade);
+                res.status(201).json(trade);
+            }catch(err){
+                res.status(500).json(err);
+            }
+        }
+        case "GET":{
+            try{
+                let { db } = await connectToDatabase();
+                const trades = await db.collection("checklist").find().toArray();
+                res.status(200).json({ trades });
+            }catch(err){
+                res.status(500).json(err);
+            }  
         }
 
     }
-    if(req.method == "GET"){
-        try{
-            let { db } = await connectToDatabase();
-            const planets = await db.collection("checklist").find().toArray();
-            res.status(200).json({ planets });
-        }catch(err){
-            res.status(500).json(err);
-        }  
- }
-}
+}   
